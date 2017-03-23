47884dbb4e2726cfb8b1f70fa0f7e1247ad608ee
Joe Nilson <joenilson@gmail.com>
2017-03-22 20:18:40 -0500

    nomina: fix menor no transportable

--- controller/configuracion_nomina.php
+++ controller/configuracion_nomina.php
@@ -284,6 +284,57 @@ class configuracion_nomina extends fs_controller{
         // formatos de presentacion
         //@TODO
 
+        //Cargar el menu
+        $this->check_menu();
+    }
+
+    /**
+     * Cargamos el menú en la base de datos, pero en varias pasadas.
+     */
+    private function check_menu() {
+        if (file_exists(__DIR__)) {
+            $max = 25;
+
+            /// leemos todos los controladores del plugin
+            foreach (scandir(__DIR__) as $f) {
+                if ($f != '.' AND $f != '..' AND is_string($f) AND strlen($f) > 4 AND ! is_dir($f) AND $f != __CLASS__ . '.php') {
+                    /// obtenemos el nombre
+                    $page_name = substr($f, 0, -4);
+
+                    /// lo buscamos en el menú
+                    $encontrado = FALSE;
+                    foreach ($this->menu as $m) {
+                        if ($m->name == $page_name) {
+                            $encontrado = TRUE;
+                            break;
+                        }
+                    }
+
+                    if (!$encontrado) {
+                        require_once __DIR__ . '/' . $f;
+                        $new_fsc = new $page_name();
+
+                        if (!$new_fsc->page->save()) {
+                            $this->new_error_msg("Imposible guardar la página " . $page_name);
+                        }
+
+                        unset($new_fsc);
+
+                        if ($max > 0) {
+                            $max--;
+                        } else {
+                            $this->recargar = TRUE;
+                            $this->new_message('Instalando las entradas al menú para el plugin... &nbsp; <i class="fa fa-refresh fa-spin"></i>');
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            $this->new_error_msg('No se encuentra el directorio ' . __DIR__);
+        }
+
+        $this->load_menu(TRUE);
     }
 
     public function importar_cargos(){
